3a579d671effb96a6636c7e05a244ec48c5b0ecb
unknown <jinhojo9482@gmail.com>
2024-03-22 09:41:44 +0900

    Declare and operate variables

---
+++ day3/day1.js
@@ -0,0 +1,20 @@
+var number = 0;
+var number = 2;
+console.log(number); // 1 (Can be redeclared by using var)
+
+// let num = 0;
+// let num = 1;
+// console.log(number); // 1 (Cannot be redeclared by using let)
+
+var str = "Hi";
+
+console.log(typeof str);
+console.log(number.toExponential(5.1234565789));
+
+var name = "Jinho";
+console.log(str, name, 1, NaN, null, undefined);
+
+var num = 50;
+var num1 = "1";
+console.log(num1 + num);
+console.log(name / num);
